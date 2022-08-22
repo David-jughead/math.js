11bf9989994d8cb0a47844766b7bd25202e8cf2d
david-jughead <makutososa300@gmail.com>
2022-08-21 22:56:26 -0400

    prototype math

--- math.js
+++ math.js
@@ -1,3 +1,5 @@
+console.group('Cuadrado')
+
 const ladoCuadrado = 5;
 const perimetroCuadrado = ladoCuadrado * 4;
 
@@ -15,6 +17,10 @@ function calcularCuadrado(lado) {
         area: lado * lado,
     }
 }
+
+console.groupEnd('Cuadrado')
+
+console.group('Triangulo')
 
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
@@ -39,3 +45,34 @@ console.log({
     perimetroTriangulo,
     areaTriangulo,
 })
+
+console.groupEnd('Triandgulo')
+
+console.group('Circle')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circuferencia = diametroCirculo * PI;
+const areacirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circuferencia,
+    areacirculo,
+});
+
+function calcularCirculo(radio) {
+    const diametro = radio * 2;
+    const radioAlcuadrado = Math.pow(radio, 2);
+    
+    return {
+        circuferencia: diametro * Math.PI,
+        area: radioAlcuadrado * Math.PI,
+    };
+} 
+
+console.groupEnd('Circle')
