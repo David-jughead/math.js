fc02eb93711d4fe4ee01dcff05cab78fce69ac1f
david-jughead <makutososa300@gmail.com>
2022-08-23 23:08:32 -0400

    mediana impar

--- platzimath.js
+++ platzimath.js
@@ -1,15 +1,28 @@
-function calcularPromedio(lista) {
-    // let sumaLista = 0;
-    // for (let i = 0; i < lista.length; i++) {
-    //     sumaLista = sumaLista + lista[i];
-    // }
+function esPar(lista) {
+    return !(lista.length % 2);
+}
+function esImpar(lista) {
+    return lista.length % 2;
+}
 
+function calcularMediana(lista) {
+    const listaEsPar = esPar(lista);
+
+    if (listaEsPar) {
+        // ...
+    } else {
+        const indexMitadListaImpar =  Math.floor(lista.length / 2);
+        console.log(indexMitadListaImpar);
+        console.log(lista[indexMitadListaImpar]);
+    }
+}
+
+function calcularPromedio(lista) {
     function sumarTodosElementos(valorAcumulado, nuevoValor) {
         return valorAcumulado + nuevoValor;
     }
 
     const sumaLista = lista.reduce(sumarTodosElementos);
-
     const promedio = sumaLista / lista.length;
     console.log(promedio)
     return promedio;
