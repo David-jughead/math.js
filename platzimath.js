e9865d27b20de8c920be0e463c312ab8ba355543
david-jughead <makutososa300@gmail.com>
2022-08-24 18:38:10 -0400

    add sort

--- platzimath.js
+++ platzimath.js
@@ -5,7 +5,8 @@ function esImpar(lista) {
     return lista.length % 2;
 }
 
-function calcularMediana(lista) {
+function calcularMediana(listaDesordenada) {
+    const lista = ordenarLista(listaDesordenada);
     const listaEsPar = esPar(lista);
 
     if (listaEsPar) {
@@ -36,4 +37,24 @@ function calcularPromedio(lista) {
     const promedio = sumaLista / lista.length;
     // console.log(promedio)
     return promedio;
+}
+
+
+function ordenarLista(listaDesordenada) {
+    function ordenarListaSort(valorAcumulado, nuevoValor) {
+        // if (valorAcumulado > nuevoValor) {
+        //     return 1;
+        // } else if (valorAcumulado == nuevoValor) {
+        //     return 0;
+        // } else if (valorAcumulado < nuevoValor) {
+        //     return -1;
+        // }
+        
+        return valorAcumulado - nuevoValor;
+    }
+    
+    // const lista = listaDesordenada.sort(ordenarListaSort);
+    const lista = listaDesordenada.sort((a,b) => a-b);
+    
+    return lista;
 }
