f7159be919ffc015b0540b17c422e1bf60960f4b
david-jughead <makutososa300@gmail.com>
2022-08-26 16:02:00 -0400

    estatic math

--- platzimath.js
+++ platzimath.js
@@ -1,11 +1,14 @@
-function esPar(lista) {
+const PlatziMath = {};
+
+PlatziMath.esPar = function esPar(lista) {
     return !(lista.length % 2);
 }
-function esImpar(lista) {
+
+PlatziMath.esImpar = function esImpar(lista) {
     return lista.length % 2;
 }
 
-function calcularModa(lista) {
+PlatziMath.calcularModa = function calcularModa(lista) {
     const listaCount = {};
 
     for (let i = 0; i < lista.length; i++) {
@@ -26,7 +29,7 @@ function calcularModa(lista) {
     return moda;
 }
 
-function calcularMediana(listaDesordenada) {
+PlatziMath.calcularMediana = function calcularMediana(listaDesordenada) {
     const lista = ordenarLista(listaDesordenada);
     const listaEsPar = esPar(lista);
 
@@ -49,7 +52,7 @@ function calcularMediana(listaDesordenada) {
     }
 }
 
-function calcularPromedio(lista) {
+PlatziMath.calcularPromedio = function calcularPromedio(lista) {
     function sumarTodosElementos(valorAcumulado, nuevoValor) {
         return valorAcumulado + nuevoValor;
     }
@@ -61,7 +64,7 @@ function calcularPromedio(lista) {
 }
 
 
-function ordenarListaBidimensional(listaDesordenada, i) {
+PlatziMath.ordenarListaBidimensional = function ordenarListaBidimensional(listaDesordenada, i) {
     function ordenarListaSort(valorAcumulado, nuevoValor) {    
         return valorAcumulado[i] - nuevoValor[i];
     }
